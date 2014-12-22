ceff962843bdc9a54e851665999352acffc0abbe
Uwe Jäger <uwej711@googlemail.com>
2014-12-22 09:40:20 +0100

    frontend integration

---
+++ Twig/ShariffExtension.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * www.valiton.com
+ *
+ * @author Uwe Jäger <uwe.jaeger@valiton.com>
+ */
+
+
+namespace Valiton\Bundle\ShariffBundle\Twig;
+
+
+use Valiton\Bundle\ShariffBundle\ShariffConfig;
+
+class ShariffExtension extends \Twig_Extension
+{
+    protected $shariffConfig;
+
+    public function __construct(ShariffConfig $shariffConfig)
+    {
+        $this->shariffConfig = $shariffConfig;
+    }
+
+
+    public function getGlobals()
+    {
+        return array('shariffConfig' => $this->shariffConfig);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'shariff';
+    }
+
+}
